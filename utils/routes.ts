7726a75925524bba31481d439fcda0e320bdf496
Lucas Caceres <lucas.caceres.lc@hotmai.com>
2024-04-04 10:08:45 -0300

    change apiurl

--- utils/routes.ts
+++ utils/routes.ts
@@ -1,5 +1,5 @@
-//const host = "https://case-router-api.onrender.com";
-const host = "http://localhost:3001";
+const host = "https://case-router-api.onrender.com";
+//const host = "http://localhost:3001";
 export const apiUrl = host + "/api";
 export const staticsUrl = host + "/fotos_casos/";
 export const staticsPdf = host + "/pdf_temp/";
