eed7863e35035aac586b80b76b8f8e4517989b50
Lucas Caceres <lucas.caceres.lc@hotmai.com>
2024-04-23 16:20:23 -0300

    servir statics

--- utils/routes.ts
+++ utils/routes.ts
@@ -1,5 +1,5 @@
 //const host = "http://localhost:3001";
 const host = process.env.NEXT_PUBLIC_API;
 export const apiUrl = host + "/backend";
-export const staticsUrl = host + "/fotos_casos/";
-export const staticsPdf = host + "/pdf_temp/";
+export const staticsUrl = host + "/backend/fotos_casos/";
+export const staticsPdf = host + "/backend/pdf_temp/";
